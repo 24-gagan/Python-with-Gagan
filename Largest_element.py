c83da3a72692e09f597ce1a8493d770e3937bad0
24-gagan <59331540+24-gagan@users.noreply.github.com>
2025-01-31 22:17:44 +0530

    Add files via upload

---
+++ Largest_element.py
@@ -0,0 +1,3 @@
+def find_max(lst):
+    return max(lst)
+print(find_max([10,11,23,34,56,78]))
---
+++ prime number.py
@@ -0,0 +1,6 @@
+num=11
+for i in range(2,num):
+    if i%2==0:
+        print("not a prime number",i)
+    else:
+        print("is a prime number",i)
